2cd4ac942ee9862f372981d0e8f9afa97cbd513c
mizunoawa.web <saeki_takatoshi@outlook.jp>
2018-01-13 18:54:03 +0900

    first commit

---
+++ ItemManager/ItemData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemManager
+{
+    public class Item
+    {
+        public string Name => "hoge";
+    }
+
+    public class Items : ObservableCollection<Item>
+    {
+        public Items()
+        {
+            Add(new Item());
+            Add(new Item());
+            Add(new Item());
+            Add(new Item());
+            Add(new Item());
+            Add(new Item());
+        }
+    }
+}
